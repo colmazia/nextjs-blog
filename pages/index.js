92d509f4830e29b293b813c18ce578fad2b96e98
colmazia <ongart.pisan@gmail.com>
2021-12-14 13:14:10 +0700

    change my self introduction

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>My name is Arm. I am new to nextjs.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
